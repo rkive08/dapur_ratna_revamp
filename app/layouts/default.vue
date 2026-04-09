c4b310cbf7f4e9bb6ab51b02385385da5a393544
rkive08 <ayuintennurillah08@gmail.com>
2026-04-09 14:29:17 +0700

    Feat: First commit

---
+++ app/layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="min-h-screen flex flex-col">
+        <header class="bg-gray-800 text-white p-4">
+            <h1 class="text-2xl">Dapur Ratna</h1>
+        </header>
+        <main class="flex-grow">
+            <slot />
+        </main>
+    </div>
+</template>
